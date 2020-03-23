2dce87bdd13034c01c9028662f7597625d65bfdf
Vicente Jesus Lechuga <vicho0202@live.com>
2020-03-23 18:51:04 -0300

    Added project files

---
+++ IDE2YTYP/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace IDE2YTYP
+{
+    partial class IDE2YTYP
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browse_ide = new System.Windows.Forms.Button();
+            this.ide_textbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ydr_textbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.out_textbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.browse_ydr = new System.Windows.Forms.Button();
+            this.browse_out = new System.Windows.Forms.Button();
+            this.convert_button = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lodist_textbox = new System.Windows.Forms.TextBox();
+            this.hdtexturedist_textbox = new System.Windows.Forms.TextBox();
+            this.s = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.porcentaje = new System.Windows.Forms.Label();
+            this.d = new System.Windows.Forms.GroupBox();
+            this.d.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // browse_ide
+            // 
+            this.browse_ide.Location = new System.Drawing.Point(546, 33);
+            this.browse_ide.Name = "browse_ide";
+            this.browse_ide.Size = new System.Drawing.Size(75, 20);
+            this.browse_ide.TabIndex = 0;
+            this.browse_ide.Text = "Browse";
+            this.browse_ide.UseVisualStyleBackColor = true;
+            this.browse_ide.Click += new System.EventHandler(this.browse_ide_Click);
+            // 
+            // ide_textbox
+            // 
+            this.ide_textbox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ide_textbox.Location = new System.Drawing.Point(190, 33);
+            this.ide_textbox.Name = "ide_textbox";
+            this.ide_textbox.Size = new System.Drawing.Size(350, 20);
+            this.ide_textbox.TabIndex = 1;
+            this.ide_textbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.ide_textbox.TextChanged += new System.EventHandler(this.ide_textbox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(127, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IDE Folder";
+            // 
+            // ydr_textbox
+            // 
+            this.ydr_textbox.Location = new System.Drawing.Point(190, 59);
+            this.ydr_textbox.Name = "ydr_textbox";
+            this.ydr_textbox.Size = new System.Drawing.Size(350, 20);
+            this.ydr_textbox.TabIndex = 1;
+            this.ydr_textbox.TextChanged += new System.EventHandler(this.ydr_textbox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(122, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "YDR Folder";
+            // 
+            // out_textbox
+            // 
+            this.out_textbox.Location = new System.Drawing.Point(190, 85);
+            this.out_textbox.Name = "out_textbox";
+            this.out_textbox.Size = new System.Drawing.Size(350, 20);
+            this.out_textbox.TabIndex = 1;
+            this.out_textbox.TextChanged += new System.EventHandler(this.out_textbox_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(113, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Output Folder";
+            // 
+            // browse_ydr
+            // 
+            this.browse_ydr.Location = new System.Drawing.Point(546, 59);
+            this.browse_ydr.Name = "browse_ydr";
+            this.browse_ydr.Size = new System.Drawing.Size(75, 20);
+            this.browse_ydr.TabIndex = 3;
+            this.browse_ydr.Text = "Browse";
+            this.browse_ydr.UseVisualStyleBackColor = true;
+            this.browse_ydr.Click += new System.EventHandler(this.browse_ydr_Click);
+            // 
+            // browse_out
+            // 
+            this.browse_out.Location = new System.Drawing.Point(546, 85);
+            this.browse_out.Name = "browse_out";
+            this.browse_out.Size = new System.Drawing.Size(75, 20);
+            this.browse_out.TabIndex = 4;
+            this.browse_out.Text = "Browse";
+            this.browse_out.UseVisualStyleBackColor = true;
+            this.browse_out.Click += new System.EventHandler(this.browse_out_Click);
+            // 
+            // convert_button
+            // 
+            this.convert_button.Location = new System.Drawing.Point(300, 122);
+            this.convert_button.Name = "convert_button";
+            this.convert_button.Size = new System.Drawing.Size(150, 50);
+            this.convert_button.TabIndex = 5;
+            this.convert_button.Text = "Convert";
+            this.convert_button.UseVisualStyleBackColor = true;
+            this.convert_button.Click += new System.EventHandler(this.convert_button_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(113, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "LOD Distance";
+            // 
+            // lodist_textbox
+            // 
+            this.lodist_textbox.Location = new System.Drawing.Point(102, 138);
+            this.lodist_textbox.MaxLength = 5;
+            this.lodist_textbox.Name = "lodist_textbox";
+            this.lodist_textbox.Size = new System.Drawing.Size(100, 20);
+            this.lodist_textbox.TabIndex = 8;
+            this.lodist_textbox.Text = "20000";
+            this.lodist_textbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // hdtexturedist_textbox
+            // 
+            this.hdtexturedist_textbox.Location = new System.Drawing.Point(517, 138);
+            this.hdtexturedist_textbox.MaxLength = 5;
+            this.hdtexturedist_textbox.Name = "hdtexturedist_textbox";
+            this.hdtexturedist_textbox.Size = new System.Drawing.Size(100, 20);
+            this.hdtexturedist_textbox.TabIndex = 8;
+            this.hdtexturedist_textbox.Text = "200";
+            this.hdtexturedist_textbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // s
+            // 
+            this.s.AutoSize = true;
+            this.s.Location = new System.Drawing.Point(514, 122);
+            this.s.Name = "s";
+            this.s.Size = new System.Drawing.Size(107, 13);
+            this.s.TabIndex = 7;
+            this.s.Text = "HD Texture Distance";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(644, 59);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(32, 20);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 9;
+            this.progressBar1.Visible = false;
+            // 
+            // porcentaje
+            // 
+            this.porcentaje.AutoSize = true;
+            this.porcentaje.Location = new System.Drawing.Point(641, 37);
+            this.porcentaje.Name = "porcentaje";
+            this.porcentaje.Size = new System.Drawing.Size(21, 13);
+            this.porcentaje.TabIndex = 10;
+            this.porcentaje.Text = "0%";
+            this.porcentaje.Visible = false;
+            // 
+            // d
+            // 
+            this.d.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.d.AutoSize = true;
+            this.d.Controls.Add(this.ide_textbox);
+            this.d.Controls.Add(this.porcentaje);
+            this.d.Controls.Add(this.label1);
+            this.d.Controls.Add(this.progressBar1);
+            this.d.Controls.Add(this.label2);
+            this.d.Controls.Add(this.lodist_textbox);
+            this.d.Controls.Add(this.hdtexturedist_textbox);
+            this.d.Controls.Add(this.label3);
+            this.d.Controls.Add(this.browse_out);
+            this.d.Controls.Add(this.label4);
+            this.d.Controls.Add(this.s);
+            this.d.Controls.Add(this.out_textbox);
+            this.d.Controls.Add(this.ydr_textbox);
+            this.d.Controls.Add(this.convert_button);
+            this.d.Controls.Add(this.browse_ide);
+            this.d.Controls.Add(this.browse_ydr);
+            this.d.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.d.Location = new System.Drawing.Point(12, 12);
+            this.d.Name = "d";
+            this.d.Size = new System.Drawing.Size(760, 192);
+            this.d.TabIndex = 11;
+            this.d.TabStop = false;
+            this.d.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // IDE2YTYP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(784, 221);
+            this.Controls.Add(this.d);
+            this.HelpButton = true;
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.Name = "IDE2YTYP";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IDE2YTYP";
+            this.Load += new System.EventHandler(this.IDE2YTYP_Load);
+            this.d.ResumeLayout(false);
+            this.d.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button browse_ide;
+        private System.Windows.Forms.TextBox ide_textbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ydr_textbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox out_textbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button browse_ydr;
+        private System.Windows.Forms.Button browse_out;
+        private System.Windows.Forms.Button convert_button;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox lodist_textbox;
+        private System.Windows.Forms.TextBox hdtexturedist_textbox;
+        private System.Windows.Forms.Label s;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label porcentaje;
+        private System.Windows.Forms.GroupBox d;
+    }
+}
+
---
+++ IDE2YTYP/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using CodeWalker.GameFiles;
+using SharpDX;
+
+
+namespace IDE2YTYP
+{
+    public partial class IDE2YTYP : Form
+    {
+        
+        public static string folderide;
+        public static string folderydr;
+        public static string folderout;
+
+        public static StringBuilder missing = new StringBuilder();
+
+
+        public static bool ideempty;
+        public static bool ydrempty;
+        public static bool outempty;
+
+        private static string modelName;
+        private static string textureDic;
+
+        private static string modelNamet;
+        private static string textureDict;
+
+        public static string ModelName { get => modelName; set => modelName = value; }
+        public static string TextureDic { get => textureDic; set => textureDic = value; }
+        public static string ModelNamet { get => modelNamet; set => modelNamet = value; }
+        public static string TextureDict { get => textureDict; set => textureDict = value; }
+
+        public IDE2YTYP()
+        {
+            InitializeComponent();
+            missing.Clear();
+        }
+
+
+        private void browse_ide_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "Select a folder that contains your IDEs files.";
+            fbd.ShowDialog();
+            ide_textbox.Text = fbd.SelectedPath;
+        }
+
+        private void browse_ydr_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd2 = new FolderBrowserDialog();
+            fbd2.Description = "Select a folder that contains your YDRs files.";
+
+            fbd2.ShowDialog();
+            ydr_textbox.Text = fbd2.SelectedPath;
+        }
+
+        private void browse_out_Click(object sender, EventArgs e)
+        {
+
+            FolderBrowserDialog fbd3 = new FolderBrowserDialog();
+            fbd3.Description = "Select a folder you want to output the files.";
+            fbd3.ShowDialog();
+            out_textbox.Text = fbd3.SelectedPath;
+
+        }
+
+        public void OpenIDEs(string idefolda, string ydrfolda, string outfolda)
+        {
+            bool tobj = false;
+            bool obj = false;
+             
+            bool issom = (tobj || obj);
+
+            string[] idefiles = Directory.GetFiles(idefolda, "*.ide");
+
+            MessageBox.Show(idefiles.Length + " IDEs detected", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            foreach (var ide in idefiles)
+            {
+
+                YtypFile ytf = new YtypFile();
+
+                string filename = Path.GetFileNameWithoutExtension(ide);
+
+                missing.AppendLine(filename + " missing models: ");
+
+                string[] idelines = File.ReadAllLines(ide);
+
+                foreach (var line in idelines)
+                {
+                    string fixline = line.ToLowerInvariant();
+
+                    string[] linesplitted = fixline.Split(',');
+
+                    if (!issom)
+                    {
+                        if (string.IsNullOrEmpty(fixline)) continue;
+                        if (fixline.StartsWith("#")) continue; //ignore comments
+                        if (fixline.StartsWith("objs")) { obj = true; continue; };
+                        if (fixline.StartsWith("tobj")) { tobj = true; continue; };
+                    }
+
+                    if (fixline.StartsWith("end"))
+                    {
+                        tobj = false;
+                        obj = false;
+                        continue;
+                    }
+
+                    if (obj)
+                    {
+
+                        modelName = linesplitted[1].ToString().Trim(); //ModelName in IDE
+                        textureDic = linesplitted[2].ToString().Trim(); //TextureDictionary in IDE 
+
+                        Archetype arc = new Archetype();
+
+                        arc._BaseArchetypeDef.assetName = JenkHash.GenHash(modelName);
+                        arc._BaseArchetypeDef.textureDictionary = JenkHash.GenHash(TextureDic);
+                        arc._BaseArchetypeDef.flags = 12582912;
+                        arc._BaseArchetypeDef.lodDist = float.Parse(lodist_textbox.Text);
+                        arc._BaseArchetypeDef.name = JenkHash.GenHash(modelName);
+                        arc._BaseArchetypeDef.hdTextureDist = float.Parse(hdtexturedist_textbox.Text);
+                        arc._BaseArchetypeDef.assetType = rage__fwArchetypeDef__eAssetType.ASSET_TYPE_DRAWABLE;
+                        arc._BaseArchetypeDef.bbMin = GetYDR(modelName).bbmin;
+                        arc._BaseArchetypeDef.bbMax = GetYDR(modelName).bbmax;
+                        arc._BaseArchetypeDef.bsCentre = GetYDR(modelName).bbcenter;
+                        arc._BaseArchetypeDef.bsRadius = GetYDR(modelName).bbsphere;
+
+                        ytf.AddArchetype(arc);
+                    }
+
+                    if (tobj)
+                    {
+
+                        modelNamet = linesplitted[1].ToString().Trim(); //ModelName in IDE
+                        textureDict = linesplitted[2].ToString().Trim(); //TextureDictionary in IDE 
+
+                        TimeArchetype tarc = new TimeArchetype();
+
+                        tarc._TimeArchetypeDef._BaseArchetypeDef.assetName = JenkHash.GenHash(modelNamet);
+                        tarc._TimeArchetypeDef._BaseArchetypeDef.textureDictionary = JenkHash.GenHash(TextureDict);
+                        tarc._TimeArchetypeDef._BaseArchetypeDef.flags = 12582912;
+                        tarc._TimeArchetypeDef._BaseArchetypeDef.lodDist = float.Parse(lodist_textbox.Text);
+                        tarc._TimeArchetypeDef._BaseArchetypeDef.name = JenkHash.GenHash(modelNamet);
+                        tarc._TimeArchetypeDef._BaseArchetypeDef.hdTextureDist = float.Parse(hdtexturedist_textbox.Text);
+                        tarc._TimeArchetypeDef._BaseArchetypeDef.assetType = rage__fwArchetypeDef__eAssetType.ASSET_TYPE_DRAWABLE;
+                        tarc._TimeArchetypeDef._BaseArchetypeDef.bbMax = GetYDR(modelNamet).bbmax;
+                        tarc._TimeArchetypeDef._BaseArchetypeDef.bbMin = GetYDR(modelNamet).bbmin;
+                        tarc._TimeArchetypeDef._BaseArchetypeDef.bsCentre = GetYDR(modelNamet).bbcenter;
+                        tarc._TimeArchetypeDef._BaseArchetypeDef.bsRadius = GetYDR(modelNamet).bbsphere;
+                        tarc._TimeArchetypeDef._TimeArchetypeDef.timeFlags = 33030175;
+
+                        ytf.AddArchetype(tarc);
+                    }
+
+                    //bw.ReportProgress((int)(progresoide * 100 / idelines.Length));
+
+                }
+                byte[] newData = ytf.Save();
+                File.WriteAllBytes(folderout + "//" + filename + ".ytyp", newData);
+            }
+                      
+        }
+
+        private void convert_button_Click(object sender, EventArgs e)
+        {	
+
+            if (Check(folderide, folderydr, folderout).Item1 && Check(folderide, folderydr, folderout).Item2 && Check(folderide, folderydr, folderout).Item3)
+            {
+                OpenIDEs(folderide, folderydr, folderout);
+                MessageBox.Show("Done", "Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                File.WriteAllText("missingmodels.txt", missing.ToString());
+            }
+        }
+
+        public (Vector3 bbmax, Vector3 bbmin, Vector3 bbcenter, float bbsphere) GetYDR(string file)
+        {
+
+            Vector3 bbmax;
+            Vector3 bbmin;
+            Vector3 bbcenter;
+            float bbsphere;
+
+            try
+            {
+                YdrFile yd = new YdrFile();
+                byte[] data = File.ReadAllBytes(folderydr + "//" + file + ".ydr");
+                RpfFile.LoadResourceFile<YdrFile>(yd, data, 165);
+
+                bbmax = yd.Drawable.BoundingBoxMax;
+                bbmin = yd.Drawable.BoundingBoxMin;
+                bbcenter = yd.Drawable.BoundingCenter;
+                bbsphere = yd.Drawable.BoundingSphereRadius;
+
+            }
+            catch (Exception)
+            {
+
+                missing.AppendLine(file);
+
+
+                YdrFile yd2 = new YdrFile();
+                byte[] data2 = File.ReadAllBytes("default.ydr");
+                RpfFile.LoadResourceFile<YdrFile>(yd2, data2, 165);
+
+                bbmax = yd2.Drawable.BoundingBoxMax;
+                bbmin = yd2.Drawable.BoundingBoxMin;
+                bbcenter = yd2.Drawable.BoundingCenter;
+                bbsphere = yd2.Drawable.BoundingSphereRadius;
+            }
+
+
+
+            return (bbmax, bbmin, bbcenter, bbsphere);
+
+        }
+
+        private void ide_textbox_TextChanged(object sender, EventArgs e)
+        {
+            folderide = ide_textbox.Text;
+        }
+
+        private void ydr_textbox_TextChanged(object sender, EventArgs e)
+        {
+            folderydr = ydr_textbox.Text;
+
+        }
+
+        private void out_textbox_TextChanged(object sender, EventArgs e)
+        {
+            folderout = out_textbox.Text;
+
+        }
+
+        private (bool, bool, bool) Check(string idepath, string ydrpath, string outpath)
+        {
+            bool thereiside = false;
+            bool thereisydr = false;
+            bool thereisout = false;
+
+            if (string.IsNullOrEmpty(idepath))
+            {
+                MessageBox.Show("IDE Path cannot be empty, please select a path and try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                thereiside = false;
+            }
+            else
+            {
+                thereiside = true;
+
+            }
+
+            if (string.IsNullOrEmpty(ydrpath))
+            {
+                MessageBox.Show("YDR Path cannot be empty, please select a path and try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                thereisydr = false;
+            }
+            else
+            {
+                thereisydr = true;
+
+            }
+
+            if (string.IsNullOrEmpty(outpath))
+            {
+                MessageBox.Show("Output Path cannot be empty, please select a path and try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                thereisout = false;
+            }
+            else
+            {
+                thereisout = true;
+
+            }
+
+            return (thereiside, thereisydr, thereisout);
+        }
+
+        private void IDE2YTYP_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
